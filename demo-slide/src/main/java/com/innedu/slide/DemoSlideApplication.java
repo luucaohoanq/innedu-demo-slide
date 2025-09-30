48135c6ead1cd882adceefebd6738dcc11ec16e2
lcaohoanq <hoangclw@gmail.com>
2025-09-30 09:06:34 +0700

    feat: initial commit

---
+++ demo-slide/src/main/java/com/innedu/slide/DemoSlideApplication.java
@@ -0,0 +1,13 @@
+package com.innedu.slide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSlideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSlideApplication.class, args);
+    }
+
+}
---
+++ demo-slide/src/test/java/com/innedu/slide/DemoSlideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innedu.slide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSlideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
